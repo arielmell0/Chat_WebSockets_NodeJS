c6f4dd2bedf139ad2e007a904654de542fd2c1eb
arielmell0 <arielmello@rede.ulbra.br>
2022-03-02 22:24:47 -0300

    feat: configurando o nosso servidor

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+const port = 3030
+const server = require('http').createServer(app)
+const io = require('socket.io')(server)
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'public'))
+app.engine('html', require('ejs').renderFile)
+app.set('view engine', 'html')
+
+app.use('/', (req, res) => {
+    res.render('index.html')
+})
+
+app.listen(port)
